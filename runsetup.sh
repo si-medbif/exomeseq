37f560ad69a376780d4dc1b218ea3acc656e3731
Harald Grove <haraldgrove@gmail.com>
2018-08-21 09:15:22 +0700

    Temporary change to the download link for references.

--- runsetup.sh
+++ runsetup.sh
@@ -23,30 +23,35 @@ exomeseq/setup.py
 # Download reference and database files
 # Populate the resources folder with the required files
 #############################################################
-cd resources/hg38bundle
-wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.*
-docker run --rm -v $PWD:/data biocontainers/bwa bwa index /data/Homo_sapiens_assembly38.fasta.gz
-gunzip -c Homo_sapiens_assembly38.fasta.gz > Homo_sapiens_assembly38.fasta
-wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz*
-wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz*
-cd ../snpeff_db
-# dbNSFP
-wget ftp://dbnsfp:dbnsfp@dbnsfp.softgenetics.com/dbNSFPv3.5a.zip
-# Preparing the dbNSFP database: (from https://github.com/bcbio/bcbio-nextgen/issues/1571)
-unzip dbNSFPv3.5a.zip
-head -n1 dbNSFP3.5a_variant.chr1 > dbNSFPv3.5a.txt
-cat dbNSFP3.5a_variant.chr* | grep -v ^# >> dbNSFPv3.5a.txt
-bgzip dbNSFPv3.5a.txt
-tabix -s 1 -b 2 -e 2 dbNSFPv3.5a.txt.gz
-# PhastCons
-wget --recursive --no-parent -l2 http://hgdownload-test.cse.ucsc.edu/goldenPath/hg38/phastCons100way/
-mv hgdownload-test.cse.ucsc.edu/goldenPath/hg38/phastCons100way/ phastCons100way
-rm -rf hgdownload-test.cse.ucsc.edu/
-# ClinVar
-wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20180729.vcf.gz*
-# GWASCat
-wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/releases/latest/gwas-catalog-associations.tsv
-cd ../..
+
+# NB! This link is temporary and is not guaranteed to work outside of the time for the workshop, Sept 10-11.
+wget https://si-medbif-exomeseq.sgp1.digitaloceanspaces.com/exome_pipeline_resources.tar.gz
+tar xzvf exome_pipeline_resources.tar.gz
+
+#cd resources/hg38bundle
+#wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.*
+#docker run --rm -v $PWD:/data biocontainers/bwa bwa index /data/Homo_sapiens_assembly38.fasta.gz
+#gunzip -c Homo_sapiens_assembly38.fasta.gz > Homo_sapiens_assembly38.fasta
+#wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz*
+#wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz*
+#cd ../snpeff_db
+## dbNSFP
+#wget ftp://dbnsfp:dbnsfp@dbnsfp.softgenetics.com/dbNSFPv3.5a.zip
+## Preparing the dbNSFP database: (from https://github.com/bcbio/bcbio-nextgen/issues/1571)
+#unzip dbNSFPv3.5a.zip
+#head -n1 dbNSFP3.5a_variant.chr1 > dbNSFPv3.5a.txt
+#cat dbNSFP3.5a_variant.chr* | grep -v ^# >> dbNSFPv3.5a.txt
+#bgzip dbNSFPv3.5a.txt
+#tabix -s 1 -b 2 -e 2 dbNSFPv3.5a.txt.gz
+## PhastCons
+#wget --recursive --no-parent -l2 http://hgdownload-test.cse.ucsc.edu/goldenPath/hg38/phastCons100way/
+#mv hgdownload-test.cse.ucsc.edu/goldenPath/hg38/phastCons100way/ phastCons100way
+#rm -rf hgdownload-test.cse.ucsc.edu/
+## ClinVar
+#wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20180729.vcf.gz*
+## GWASCat
+#wget ftp://ftp.ebi.ac.uk/pub/databases/gwas/releases/latest/gwas-catalog-associations.tsv
+#cd ../..
 ##################################
 # SETUP Part3:
 # Download required docker images
